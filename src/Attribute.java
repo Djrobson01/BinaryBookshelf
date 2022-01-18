8883c106e3706494d2c074f4bdc64b198dfeb66c
Dominic Robson <Djrobson2001@gmail.com>
2022-01-18 12:02:58 -0600

    File containing attributes of a book

---
+++ src/Attribute.java
@@ -0,0 +1,8 @@
+/**
+ * This enumeration groups the names of constants representing the sortable attributes of Book
+ * objects.
+ */
+public enum Attribute {
+    AUTHOR, TITLE, PAGECOUNT, ID;
+  }
+  
